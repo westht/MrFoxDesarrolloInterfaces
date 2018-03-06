b6f4ba79a1c7c7de9788c8830567c3d9c6d659d8
Sergio Cuadrado <sergiocv97@gmail.com>
2018-03-06 20:47:02 +0100

    Commit 4 Casi todo

--- app/src/main/java/com/example/sergiocuadrado/mrfox/MyMain.java
+++ app/src/main/java/com/example/sergiocuadrado/mrfox/MyMain.java
@@ -3,27 +3,43 @@ package com.example.sergiocuadrado.mrfox;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class MyMain extends AppCompatActivity {
-
+    private TextView txt;
+    SwipeRefreshLayout swipe;
+    FrameLayout frame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        txt = (TextView)findViewById(R.id.mitexto);
+        frame = (FrameLayout)findViewById(R.id.miframe);
+        txt.setMovementMethod(new ScrollingMovementMethod());
+        swipe = (SwipeRefreshLayout)findViewById(R.id.miswipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onRefresh() {
+                refresh();
             }
         });
+
+
+
+
+    }
+    private void refresh(){
+        Snackbar.make(frame,"Lost your fox?",3).show();
+        swipe.setRefreshing(false);
+
     }
 
 }
